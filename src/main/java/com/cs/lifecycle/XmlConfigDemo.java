d8dbf1b5dc969d0594afd13567b26875ab7a4883
parashuram-patil <Parashuram.Patil@CS105-CS.contentserv.com>
2018-06-18 17:00:17 +0530

    Bean lifecycle implementation(init-method and destroy-method)
    -- userdefined methods can be life cycle methods, need xl config

---
+++ src/main/java/com/cs/lifecycle/XmlConfigDemo.java
@@ -0,0 +1,14 @@
+package com.cs.lifecycle;
+
+
+public class XmlConfigDemo {
+  
+  public void myInit() {
+    System.out.println("userdefined init() executed!!!");
+  }
+  
+  public void myDestroy() {
+    System.out.println("userdefined destroy() executed!!!");
+  }
+  
+}
