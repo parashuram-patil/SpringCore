4c2e78d9897788bda160dd96023b77ccba6c98fc
parashuram-patil <Parashuram.Patil@CS105-CS.contentserv.com>
2018-06-18 17:28:15 +0530

    BeanFactoryPostProcessor Implementation
    -- It gives a method postProcessBeanFactory() with bean factory as arg
    -- Spring calls this method when Bean factory is initialised

---
+++ src/main/java/com/cs/postprocessor/BeanFactoryPostProcesssor.java
@@ -0,0 +1,18 @@
+package com.cs.postprocessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanFactoryPostProcesssor implements BeanFactoryPostProcessor {
+
+  @Override
+  public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+      throws BeansException
+  {
+    System.out.println(beanFactory.getBeanDefinitionCount());
+  }
+  
+}
