4658bf264f88651965cdbfe7b34f14abc8e6e36f
parashuram-patil <Parashuram.Patil@CS105-CS.contentserv.com>
2018-06-18 17:07:58 +0530

    Bean lifecycle implementation(@PostConstruct and @PreConstruct) --its not springs featur, comes under JSR 250

---
+++ src/main/java/com/cs/lifecycle/PrePostConstructorDemo.java
@@ -0,0 +1,25 @@
+package com.cs.lifecycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrePostConstructorDemo {
+  
+  public PrePostConstructorDemo() {
+    System.out.println("No-args constructor called!!!");
+  }
+  
+  @PostConstruct
+  public void myInit() {
+    System.out.println("userdefined init() executed!!!");
+  }
+  
+  @PreDestroy
+  public void myDestroy() {
+    System.out.println("userdefined destroy() executed!!!");
+  }
+  
+}
