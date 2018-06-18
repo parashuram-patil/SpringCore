ac2f3dca8631e3c743cf766f1cdae0cb5eff3b7f
parashuram-patil <Parashuram.Patil@CS105-CS.contentserv.com>
2018-06-18 17:38:05 +0530

    Properties file Implenentation
    -- used Properties file to set beans properties

--- src/main/java/com/cs/main/MainClass.java
+++ src/main/java/com/cs/main/MainClass.java
@@ -3,15 +3,19 @@ package com.cs.main;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.cs.interfaces.Shape;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		
 		//BeanFactory context = new XmlBeanFactory(new FileSystemResource("src/main/resources/spring.xml"));
+    //ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 		
-		//Shape shape = (Shape)context.getBean("triangle");
-		//shape.draw();
+		Shape shape = (Shape)context.getBean("triangle");
+		shape.draw();
 		
 		/*Below line causes error since we have made abstract*/
 		//InteritanceDemo parentClass = (InteritanceDemo) context.getBean("parentClass");
@@ -19,7 +23,6 @@ public class MainClass {
 		//InteritanceDemo childClass = (InteritanceDemo) context.getBean("childClass");
 		//childClass.display();
 		
-		context.registerShutdownHook();
 	}
 
 }
