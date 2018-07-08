cd6730bf90c87cb14de46261aa449d4469bcf566
parashuram-cs <parashuram@contentsphere.com>
2018-07-08 16:55:24 +0530

    Custom Event listener to catch specific events

--- src/main/java/com/cs/eventhandling/MyApplicationEventListener.java
+++ src/main/java/com/cs/eventhandling/MyApplicationEventListener.java
@@ -1,14 +1,13 @@
 package com.cs.eventhandling;
 
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyApplicationEventListener implements ApplicationListener {
+public class MyApplicationEventListener implements ApplicationListener<MyEvent> {
 
 	@Override
-	public void onApplicationEvent(ApplicationEvent event) {
+	public void onApplicationEvent(MyEvent event) {
 		System.out.println(event.toString() + " and caught by ApplicationListener");
 	}
 
