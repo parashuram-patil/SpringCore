92186d7b7ed8830c07e4ef1628c02e2173415206
Parashuram <parasharam_patil@ymail.com>
2018-07-08 14:11:42 +0530

    Message Source Implementation -- Spring.xml configuration -- Locale specific messges -- Message arguments

--- src/main/java/com/cs/main/MainClass.java
+++ src/main/java/com/cs/main/MainClass.java
@@ -1,24 +1,24 @@
 package com.cs.main;
 
+import java.util.Locale;
+
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.cs.implementations.AutowiredTriangle;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		
 		//BeanFactory context = new XmlBeanFactory(new FileSystemResource("src/main/resources/spring.xml"));
-    //ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		context.registerShutdownHook();
 		
 		/*Shape shape = (Shape)context.getBean("triangle");
 		shape.draw();*/
 		
-		AutowiredTriangle shape = (AutowiredTriangle)context.getBean("autowiredTriangle");
-    shape.draw();
+		//AutowiredTriangle shape = (AutowiredTriangle)context.getBean("autowiredTriangle");
+        //shape.draw();
 		
 		
 		/*Below line causes error since we have made abstract*/
@@ -26,7 +26,12 @@ public class MainClass {
 		
 		//InteritanceDemo childClass = (InteritanceDemo) context.getBean("childClass");
 		//childClass.display();
-		
+
+		 Long currentTimeMillis = System.currentTimeMillis();
+		 String messageEN = context.getMessage("greeting", new String[] {currentTimeMillis.toString(), "Good Morning"}, "default Greeting", Locale.ENGLISH);
+		 String messageFR = context.getMessage("greeting", null, "default Greeting", Locale.FRENCH);
+	     System.out.println(messageEN);
+	     System.out.println(messageFR);
 	}
 
 }
