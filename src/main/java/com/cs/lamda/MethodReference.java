53b89588d7bea9352b20df13b9969f66fa5502fb
parashuram-cs <parashuram@contentsphere.com>
2018-07-21 15:55:17 +0530

    Lamda : Method Reference -- parameter as target

--- src/main/java/com/cs/lamda/MethodReference.java
+++ src/main/java/com/cs/lamda/MethodReference.java
@@ -22,18 +22,27 @@ public class MethodReference {
   
   public void execute()
   {
-    List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5, 6 ,7, 8, 9, 0);
+    List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
     
-    /*non static method reference*/
+    /*instance method reference*/
+    System.out.println("Parameter as an argument");
     intList.forEach(MethodReference.methodReference::print);
-    //intList.forEach(System.out::print);
-    
-    System.out.println();
+    // intList.forEach(System.out::print);
     
     /*static method reference*/
+    System.out.println("\nParameter as an argument to static method");
     intList.stream()
-           .map(num -> String.valueOf(num))
-           .forEach(MethodReference::print);
+        // .map(num -> String.valueOf(num))
+        .map(String::valueOf)
+        .forEach(MethodReference::print);
+    
+    /*parameter as target*/
+    System.out.println("\nParameter as target");
+    intList.stream()
+        // here num isd used as target to invoke toString()
+        .map(num -> num.toString())
+        // .map(String::toString)
+        .forEach(MethodReference::print);
   }
   
 }
